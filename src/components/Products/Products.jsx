285088d92f61ef066510b944eb6a7e6944a95d60
p-koskey <patiencekoskey@gmail.com>
2021-04-28 23:34:49 +0300

    create and style Navbar

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import {Grid} from '@material-ui/core';
 import Product from './Product/Product'
+import useStyles from './styles'
+
 const products = [
     {
         id:1, name: 'Mua Pack 1', description: '5 pairs eyelashes', price : 'Ksh.800', image : 'https://www.byrdie.com/thmb/XPm3S0ZLBTGav8ETinGl5e7q9FA=/900x0/filters:no_upscale():max_bytes(150000):strip_icc()/s2203487-main-zoom-23e8d0b3c2994191891afbb4e06ec376.jpg'
@@ -19,8 +21,10 @@ const products = [
     },
 ]
 const Products = () => {
+    const classes = useStyles();
     return(
-   <main>
+   <main className={classes.content}>
+       <div className={classes.toolbar}/>
        <Grid container justify="center" spacing={4}>
         {products.map((product) => (
                <Grid item key={product.id} xs = {12} sm = {6} md={4} lg={3}>
