4b7622fc36de3d9b4b683db44f8dbccb0c40b2b5
p-koskey <patiencekoskey@gmail.com>
2021-04-28 19:48:51 +0300

    create Products and add Mock products for display

---
+++ src/components/Products/Products.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import {Grid} from '@material-ui/core';
+import Product from './Product/Product'
+const products = [
+    {
+        id:1, name: 'Mua Pack 1', description: '5 pairs eyelashes', price : 'Ksh.800', image : 'https://www.byrdie.com/thmb/XPm3S0ZLBTGav8ETinGl5e7q9FA=/900x0/filters:no_upscale():max_bytes(150000):strip_icc()/s2203487-main-zoom-23e8d0b3c2994191891afbb4e06ec376.jpg'
+    },
+    {
+        id:2, name: 'Mua Pack 2', description: '5 pairs eyelashes', price : 'Ksh.800', image : 'https://www.byrdie.com/thmb/XPm3S0ZLBTGav8ETinGl5e7q9FA=/900x0/filters:no_upscale():max_bytes(150000):strip_icc()/s2203487-main-zoom-23e8d0b3c2994191891afbb4e06ec376.jpg'
+    },
+    {
+        id:3, name: 'Assorted Pack 100', description: '5 pairs mixed style eyelashes', price : 'Ksh.800', image : 'https://www.byrdie.com/thmb/XPm3S0ZLBTGav8ETinGl5e7q9FA=/900x0/filters:no_upscale():max_bytes(150000):strip_icc()/s2203487-main-zoom-23e8d0b3c2994191891afbb4e06ec376.jpg'
+    },
+    {
+        id:4, name: 'Assorted Pack 200', description: '5 pairs mixed style eyelashes', price : 'Ksh.800', image : 'https://www.byrdie.com/thmb/XPm3S0ZLBTGav8ETinGl5e7q9FA=/900x0/filters:no_upscale():max_bytes(150000):strip_icc()/s2203487-main-zoom-23e8d0b3c2994191891afbb4e06ec376.jpg'
+    },
+     {
+        id:5, name: 'Mystique', description: '2 pairs eyelashes', price : 'Ksh.500', image : 'https://www.byrdie.com/thmb/XPm3S0ZLBTGav8ETinGl5e7q9FA=/900x0/filters:no_upscale():max_bytes(150000):strip_icc()/s2203487-main-zoom-23e8d0b3c2994191891afbb4e06ec376.jpg'
+    },
+]
+const Products = () => {
+    return(
+   <main>
+       <Grid container justify="center" spacing={4}>
+        {products.map((product) => (
+               <Grid item key={product.id} xs = {12} sm = {6} md={4} lg={3}>
+                    <Product product={product}/>
+                    </Grid>
+    ))
+        }
+       </Grid>
+   </main>
+    )
+}
+
+export default Products
